01fc5a81a48da4eaf55500df9cef440e8474e497
JesusVPerezGarrido <jv.perezgarrido@gmail.com>
2025-03-03 12:13:57 +0100

    better file creation

--- src/execution/heredoc_manager.c
+++ src/execution/heredoc_manager.c
@@ -6,13 +6,13 @@
 /*   By: jeperez- <jeperez-@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/26 13:40:04 by jeperez-          #+#    #+#             */
-/*   Updated: 2025/02/26 14:59:16 by jeperez-         ###   ########.fr       */
+/*   Updated: 2025/03/03 12:00:29 by jeperez-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "execution.h"
 
-static void heredoc_read(int fd, t_token tok)
+static void	heredoc_read(int fd, t_token tok)
 {
 	char	*rl;
 
@@ -29,20 +29,38 @@ static void heredoc_read(int fd, t_token tok)
 	free(rl);
 }
 
-static int prepare_heredoc(t_command *cmd, t_token *tok)
+static char	*create_file(t_command *cmd, t_token *tok)
+{
+	static int	index = 0;
+	char	*filename;
+	char	*index_str;
+
+	index_str = ft_itoa(index);
+	filename = ft_strjoin("/tmp/heredoc", index_str);
+	free(index_str);
+	if (!filename)
+	{
+		ft_fprintf(2, "Malloc error D:\n");
+		return (NULL);
+	}
+	index++;
+	if (access(filename, F_OK) == 0)
+	{
+		free(filename);
+		filename = create_file(cmd, tok);
+	}
+	return (filename);
+}
+
+static int	prepare_heredoc(t_command *cmd, t_token *tok)
 {
 	int		fd;
 	char	*filename;
 
 	if (ft_strcmp(tok->type, "heredoc"))
 		return (0);
-	filename = ft_freejoin(ft_strjoin(cmd->name, tok->type), tok->content);
-	if (!filename)
-	{
-		ft_fprintf(2, "Malloc error D:\n");
-		return (-1);
-	}
-	fd = open(filename, O_CREAT|O_WRONLY, 00644); //to-do revise heredoc name
+	filename = create_file(cmd, tok);
+	fd = open(filename, O_CREAT | O_WRONLY, 00644);
 	if (fd == -1)
 	{
 		free(filename);
